3c2f8c6d71e59c5b95184d13d31d45372e408acc
ko1596 <ko89022189@gmail.com>
2021-08-26 17:47:52 +0800

    typesetting

--- displayLCD.c
+++ displayLCD.c
@@ -17,12 +17,12 @@
 #define ROW_BYTE_NUM (1600 * 3)
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
-#define GPIO_DCX		85
-#define GPIO_RESX		86
-#define GPIO_D0			87
-#define GPIO_D1			88
-#define GPIO_D2			89
-#define GPIO_CS0		8
+#define GPIO_DCX 85
+#define GPIO_RESX 86
+#define GPIO_D0 87
+#define GPIO_D1 88
+#define GPIO_D2 89
+#define GPIO_CS0 8
 
 static const char *device = "/dev/spidev1.0";
 static uint32_t mode;
@@ -35,7 +35,6 @@ static int verbose;
 static int transfer_size;
 static int iterations;
 static int interval = 5; /* interval in seconds for showing transfer rate */
-
 
 static void pabort(const char *s)
 {
@@ -71,7 +70,7 @@ void unexport_gpio(void)
 }
 
 static void hex_dump(const void *src, size_t length, size_t line_size,
-		     char *prefix)
+					 char *prefix)
 {
 	int i = 0;
 	const unsigned char *address = src;
@@ -79,15 +78,19 @@ static void hex_dump(const void *src, size_t length, size_t line_size,
 	unsigned char c;
 
 	printf("%s | ", prefix);
-	while (length-- > 0) {
+	while (length-- > 0)
+	{
 		printf("%02X ", *address++);
-		if (!(++i % line_size) || (length == 0 && i % line_size)) {
-			if (length == 0) {
+		if (!(++i % line_size) || (length == 0 && i % line_size))
+		{
+			if (length == 0)
+			{
 				while (i++ % line_size)
 					printf("__ ");
 			}
 			printf(" |");
-			while (line < address) {
+			while (line < address)
+			{
 				c = *line++;
 				printf("%c", (c < 32 || c > 126) ? '.' : c);
 			}
@@ -119,7 +122,8 @@ static void transfer(int fd, uint8_t const *tx, uint8_t const *rx, size_t len)
 		tr.rx_nbits = 4;
 	else if (mode & SPI_RX_DUAL)
 		tr.rx_nbits = 2;
-	if (!(mode & SPI_LOOP)) {
+	if (!(mode & SPI_LOOP))
+	{
 		if (mode & (SPI_TX_QUAD | SPI_TX_DUAL))
 			tr.rx_buf = 0;
 		else if (mode & (SPI_RX_QUAD | SPI_RX_DUAL))
@@ -139,11 +143,13 @@ void LCD_WrCmd(unsigned char cmd)
 	int fd;
 
 	uint8_t tx[] = {cmd};
-	uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+	uint8_t rx[ARRAY_SIZE(tx)] = {
+		0,
+	};
 
 	gpio_SetValue(GPIO_DCX, GPIO_VALUE_LOW);
 	gpio_SetValue(GPIO_CS0, GPIO_VALUE_LOW);
-	
+
 	fd = open(device, O_RDWR);
 
 	ret = ioctl(fd, SPI_IOC_WR_MODE32, &mode);
@@ -157,7 +163,6 @@ void LCD_WrCmd(unsigned char cmd)
 	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
 	if (ret == -1)
 		pabort("can't set max speed hz");
-
 
 	transfer(fd, tx, rx, sizeof(tx));
 	gpio_SetValue(GPIO_DCX, GPIO_VALUE_HIGH);
@@ -171,7 +176,9 @@ void LCD_WrDat(uint8_t dat)
 	int fd;
 
 	uint8_t tx[] = {dat};
-	uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+	uint8_t rx[ARRAY_SIZE(tx)] = {
+		0,
+	};
 
 	gpio_SetValue(GPIO_CS0, GPIO_VALUE_LOW);
 
@@ -189,7 +196,6 @@ void LCD_WrDat(uint8_t dat)
 	if (ret == -1)
 		pabort("can't set max speed hz");
 
-
 	transfer(fd, tx, rx, sizeof(tx));
 	gpio_SetValue(GPIO_CS0, GPIO_VALUE_HIGH);
 	close(fd);
@@ -200,8 +206,9 @@ void LCD_WrPICDat(unsigned char const *data)
 	int ret = 0;
 	int fd;
 
-	
-	uint8_t rx[ARRAY_SIZE(data)] = {0, };
+	uint8_t rx[ARRAY_SIZE(data)] = {
+		0,
+	};
 	//printf("%d\n", sizeof(data));
 	gpio_SetValue(GPIO_CS0, GPIO_VALUE_LOW);
 
@@ -219,9 +226,8 @@ void LCD_WrPICDat(unsigned char const *data)
 	if (ret == -1)
 		pabort("can't set max speed hz");
 
-
 	transfer(fd, data, rx, sizeof(data));
-	
+
 	gpio_SetValue(GPIO_CS0, GPIO_VALUE_HIGH);
 	close(fd);
 }
@@ -260,19 +266,19 @@ void LCD_Init(void)
 {
 	gpio_SetValue(GPIO_CS0, GPIO_VALUE_HIGH);
 	gpio_SetValue(GPIO_RESX, GPIO_VALUE_LOW);
-	usleep( 50000 );   //  50ms
+	usleep(50000); //  50ms
 	gpio_SetValue(GPIO_RESX, GPIO_VALUE_HIGH);
-	usleep( 50000 );   //  50ms
+	usleep(50000); //  50ms
 	gpio_SetValue(GPIO_DCX, GPIO_VALUE_HIGH);
 
 	LCD_SetCmd3(0xFF, 0x21, 0x71, 0x41);
 	LCD_SetCmd1(0x05, 0x00);
 	LCD_SetCmd3(0xFF, 0x21, 0x71, 0x00);
-	LCD_SetCmd3(0xFF,0x21,0x71,0x11);
-	LCD_SetCmd1(0x0A,0x00);
-	LCD_SetCmd1(0x0B,0x18);
-	LCD_SetCmd1(0x0C,0x18);
-	LCD_SetCmd3(0xFF,0x21,0x71,0x00);
+	LCD_SetCmd3(0xFF, 0x21, 0x71, 0x11);
+	LCD_SetCmd1(0x0A, 0x00);
+	LCD_SetCmd1(0x0B, 0x18);
+	LCD_SetCmd1(0x0C, 0x18);
+	LCD_SetCmd3(0xFF, 0x21, 0x71, 0x00);
 	LCD_SetCmd1(0x38, 0x00);
 	LCD_SetCmd1(0x39, 0x1F);
 	LCD_SetCmd1(0x36, 0x08);
@@ -280,9 +286,7 @@ void LCD_Init(void)
 	LCD_SetCmd2(0x2A, 0x00, 0x85);
 	LCD_SetCmd4(0x2B, 0x00, 0x00, 0x04, 0xAF);
 
-
-
-	LCD_SetCmd3(0xFF,0x21,0x71,0x45);
+	LCD_SetCmd3(0xFF, 0x21, 0x71, 0x45);
 	LCD_SetCmd1(0x00, 0x04);
 	LCD_SetCmd1(0x01, 0xB0);
 	LCD_SetCmd1(0x07, 0x10);
@@ -331,7 +335,7 @@ void LCD_Init(void)
 	LCD_SetCmd1(0x03, 0x04);
 	LCD_SetCmd3(0xFF, 0x21, 0x71, 0x00);
 
-	usleep( 50000 );   //  50ms
+	usleep(50000); //  50ms
 	LCD_WrCmd(0x11);
 }
 
@@ -340,64 +344,65 @@ void LCD_Image(unsigned char data[])
 	int x, y, i, j;
 	unsigned char rgb[3] = {0xFF, 0x00, 0x00};
 	unsigned char r, g, b;
-	unsigned char buf ;
+	unsigned char buf;
 	unsigned char trd[482400];
 	unsigned char buff[32];
 	int count = 0;
-	LCD_WrCmd(0x2C);  
+	LCD_WrCmd(0x2C);
 	gpio_SetValue(GPIO_CS0, GPIO_VALUE_LOW);
-#if 1
-	for (y = 1199; y >= 0; y--){
-		
-		for(x = 0; x < 1600 / 4; x++){
+
+	for (y = 1199; y >= 0; y--)
+	{
+
+		for (x = 0; x < 1600 / 4; x++)
+		{
 			buf = 0;
-			for (i = 0; i < 4; i++){
+			for (i = 0; i < 4; i++)
+			{
 				//b = data[y * ROW_BYTE_NUM + (399 - x) * 3 * 4 + i * 3];
 				//g = data[y * ROW_BYTE_NUM + (399 - x) * 3 * 4 + i * 3 + 1];
 				//r = data[y * ROW_BYTE_NUM + (399 - x) * 3 * 4 + i * 3 + 2];
-				b = data[y * ROW_BYTE_NUM + (x) * 3 * 4 + i * 3];
-				g = data[y * ROW_BYTE_NUM + (x) * 3 * 4 + i * 3 + 1];
-				r = data[y * ROW_BYTE_NUM + (x) * 3 * 4 + i * 3 + 2];
+				b = data[y * ROW_BYTE_NUM + (x)*3 * 4 + i * 3];
+				g = data[y * ROW_BYTE_NUM + (x)*3 * 4 + i * 3 + 1];
+				r = data[y * ROW_BYTE_NUM + (x)*3 * 4 + i * 3 + 2];
 
 				if (b > 0x7F && g > 0x7F && r > 0x7F)
 					//buf |= 0x0 << ((3 - i) * 2);
-					buf |= 0x1 << ((i) * 2);
+					buf |= 0x1 << ((i)*2);
 
 				if (b <= 0x7F && g <= 0x7F && r > 0x7F)
 					//buf |= 0x2 << ((3 - i) * 2);
-					buf |= 0x2 << ((i) * 2);
+					buf |= 0x2 << ((i)*2);
 
 				if (b <= 0x7F && g <= 0x7F && r <= 0x7F)
 					//buf |= 0x3 << ((3 - i) * 2);
-					buf |= 0x3 << ((i) * 2);
+					buf |= 0x3 << ((i)*2);
 			}
 			//LCD_WrDat(buf);
-            //LCD_WrPICDat(buf); // R
+			//LCD_WrPICDat(buf); // R
 			trd[count++] = buf;
-			
-			
 		}
 		//LCD_WrDat(0x00);
 		trd[count++] = 0x00;
-		
+
 		//LCD_WrDat(0x00);
 		//LCD_WrPICDat(0x00);
 		trd[count++] = 0x00;
-		
 	}
 	//printf("%d\n", sizeof(trd));
 	int ret = 0;
 	int fd;
 
-	for(int ct = 0; ct < 15075; ct++)
+	for (int ct = 0; ct < 15075; ct++)
 	{
-		for(int ctj = 0; ctj < 32; ctj++)
+		for (int ctj = 0; ctj < 32; ctj++)
 		{
-			buff[ctj] = trd[ct*32 + ctj];
+			buff[ctj] = trd[ct * 32 + ctj];
 		}
-		uint8_t rx[ARRAY_SIZE(buff)] = {0, };
+		uint8_t rx[ARRAY_SIZE(buff)] = {
+			0,
+		};
 		//printf("%d\n", sizeof(buff));
-		
 
 		fd = open(device, O_RDWR);
 
@@ -413,31 +418,15 @@ void LCD_Image(unsigned char data[])
 		if (ret == -1)
 			pabort("can't set max speed hz");
 
-
 		transfer(fd, buff, rx, sizeof(buff));
-		
-		
+
 		close(fd);
 	}
-	
-	
-#endif
-
-
-#if 0
-	for (i = 0; i < 1200; i++){
-		if (400 % 3)
-			x = (400 / 3 + 1) * 3;
-		for (j = 0; j < x; j++){
-			LCD_WrDat(0xFF);
-		}
-	}
-#endif
 }
 
 int main(int argc, char **argv)
 {
-	unsigned char i=0;    
+	unsigned char i = 0;
 	unsigned char rgb[3] = {0x00, 0x00, 0xFF};
 	unsigned char *buf;
 	FILE *fp;
@@ -446,55 +435,39 @@ int main(int argc, char **argv)
 	init_gpio();
 
 	LCD_Init();
-	usleep( 120000 );   //  	120ms
+	usleep(120000); //  	120ms
 
 	//LCD_SetCmd1(0xE4, 0x02); //設定QSPI/SPI
 	LCD_WrCmd(0x29);
 
 	buf = malloc(sizeof(unsigned char) * 1600 * 1200 * 3);
-	if (buf == NULL){
+	if (buf == NULL)
+	{
 		printf("malloc error\n");
 		return 0;
 	}
 
-	#if 1
-	while(1){
+	while (1)
+	{
 		for (i = 1; i < argc; i++)
 		{
-		//if (digitalRead(4) == HIGH) 
-		
-			if (1)
+			fp = fopen(argv[i], "rb");
+			if (fp == NULL)
 			{
-	//	printf("==============================\n");
-				//LCD_WrCmd(0x28);
-				//LCD_WrCmd(0x00);
-				//usleep( 50000 );   //  	50ms
-
-	//			//sprintf(argv[i], "%d.bmp", argv[i]);
-	//			printf("4,23--> %s,%s\n", digitalRead(4),digitalRead (23));
-
-				fp = fopen(argv[i], "rb");
-				if (fp == NULL){
-					printf("open %s file error\n", str);
-					return 0;
-				}
-
-				fseek(fp, 54, SEEK_SET);
-				fread(buf, sizeof(unsigned char), 1600 * 1200 * 3, fp);
-				fclose(fp);
-
-				LCD_Image(buf);  
-				
-				usleep( 5000000 );   //  	5s
+				printf("open %s file error\n", str);
+				return 0;
 			}
-			usleep( 100000 );   //  	50ms
+
+			fseek(fp, 54, SEEK_SET);
+			fread(buf, sizeof(unsigned char), 1600 * 1200 * 3, fp);
+			fclose(fp);
+
+			LCD_Image(buf);
+
+			usleep(5000000); //  	5s
 		}
 	}
-#endif
 
 	free(buf);
-
-
 	unexport_gpio();
-
 }
